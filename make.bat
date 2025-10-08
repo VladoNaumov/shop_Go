32f07eced46baf3e5f4b87bb65b3c507a7b78f96
umbre <umbrella.spb@hotmail.com>
2025-10-08 14:37:25 +0300

    refactoring

--- make.bat
+++ make.bat
@@ -11,23 +11,23 @@ if "%1"=="" (
 )
 
 if "%1"=="run" (
-    echo 🔹 Running app on %HTTP_ADDR%...
+    echo Running app on %HTTP_ADDR%...
     go run ./cmd/app
     exit /b
 )
 
 if "%1"=="build" (
-    echo 🔹 Building binary...
+    echo Building binary...
     if not exist bin mkdir bin
     go build -o bin\app.exe ./cmd/app
-    echo ✅ Build complete: bin\app.exe
+    echo Build complete: bin\app.exe
     exit /b
 )
 
 if "%1"=="start" (
-    echo 🔹 Starting binary...
+    echo Starting binary...
     if not exist bin\app.exe (
-        echo ❌ Binary not found. Run: make build
+        echo Binary not found. Run: make build
         exit /b 1
     )
     bin\app.exe
@@ -35,33 +35,33 @@ if "%1"=="start" (
 )
 
 if "%1"=="clean" (
-    echo 🔹 Cleaning build files...
+    echo Cleaning build files...
     if exist bin rmdir /s /q bin
-    echo ✅ Clean done.
+    echo Clean done.
     exit /b
 )
 
 if "%1"=="test" (
-    echo 🔹 Running Go tests...
+    echo Running Go tests...
     go test ./... -v
     exit /b
 )
 
 if "%1"=="lint" (
-    echo 🔹 Running Go fmt and vet...
+    echo  Running Go fmt and vet...
     go fmt ./...
     go vet ./...
-    echo ✅ Lint check completed.
+    echo Lint check completed.
     exit /b
 )
 
 if "%1"=="tidy" (
-    echo 🔹 Running go mod tidy...
+    echo  Running go mod tidy...
     go mod tidy
-    echo ✅ Dependencies updated.
+    echo Dependencies updated.
     exit /b
 )
 
-echo ❌ Unknown command: %1
+echo Unknown command: %1
 echo Usage: make [run ^| build ^| start ^| clean ^| test ^| lint ^| tidy]
 exit /b 1
